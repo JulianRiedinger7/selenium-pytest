8595ef13f1d114772cb8bb750a60f02bb869ac67
Julian Riedinger <julianriedinger27@gmail.com>
2024-05-20 12:15:13 -0300

    created initial project structure

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+@pytest.fixture()
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome",
+                     help="Type of browser: Chrome, Firefox, Edge")
+
+@pytest.fixture()
+def driver(request):
+    browser_type = request.config.getoption("--browser").lower()
+
+    if browser_type == "chrome":
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+    elif browser_type == "firefox":
+        service = FirefoxService(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service)
+    elif browser_type == "edge":
+        service = EdgeService(EdgeChromiumDriverManager().install())
+        driver = webdriver.Edge(service=service)
+    else:
+        raise ValueError(f"Browser {browser_type} is not supported")
+
+    yield driver
+    driver.quit()
